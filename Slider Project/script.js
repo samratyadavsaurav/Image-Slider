a29888cce49c4aae0be92f6d0968d36c342f6b31
Saurav kumar <146668370+samratyadavsaurav@users.noreply.github.com>
2025-08-13 12:18:46 +0530

    Initial commit - slider project

---
+++ Slider Project/script.js
@@ -0,0 +1,32 @@
+let slides = document.querySelectorAll('.slide');
+let counter = 0;
+
+slides.forEach((slide , index)=>{
+  slide.style.left=`${index * 100}%`;
+  
+}
+)
+
+const goPrev = () =>{
+  if(counter > 0){
+    counter--;
+    slideImage();
+  }
+}
+
+const goNext = () =>{
+  if(counter < slides.length-1){
+    counter++;
+    slideImage();
+  }
+}
+
+
+
+const slideImage = () =>{
+  slides.forEach((slide)=>{
+     slide.style.transform=`translateX(-${counter *100}%)`;
+  })
+}
+
+setInterval(goNext,1000);
